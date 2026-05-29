5c448e7c19776ac8757769670e6b757fc249c075
tapiajs <m03tapiajs@gmail.com>
2026-05-28 20:55:43 -0600

    feat: agregar las pruebas para los procedimientos, funciones y disparadores

--- Query.sql
+++ Query.sql
@@ -129,3 +129,113 @@ SELECT IdCliente, FolioTicket, PrecioBruto, PrecioNeto FROM Ticket WHERE FolioTi
 --                              Verificar todos los resultados juntos
 --====================================================================================================
 select IdCliente, FolioTicket, PrecioBruto, PrecioNeto from Ticket;
+
+
+--====================================================================================================
+--                              Pruebas para el check Ticket_chk_reglas_web
+--====================================================================================================
+
+-- VENTA WEB SÓLO CON MEDICAMENTOS (Válido)
+INSERT INTO Ticket (FechaPago, HoraPago, TipoVenta, IdSucursal, IdCliente, EsTicketConsulta, EsTicketMedicamento)
+VALUES (CURRENT_DATE, CURRENT_TIME, 'Web', 1, 1, FALSE, TRUE);
+
+-- Resultado esperado: La consulta se ejecuta sin problemas y se crea el ticket.
+
+-- VENTA WEB DE UNA CONSULTA (Inválido)
+INSERT INTO Ticket (FechaPago, HoraPago, TipoVenta, IdSucursal, IdCliente, EsTicketConsulta, EsTicketMedicamento)
+VALUES (CURRENT_DATE, CURRENT_TIME, 'Web', 1, 1, TRUE, FALSE);
+
+-- Resultado esperado: PostgreSQL abortará la operación y lanzará este error:
+-- ERROR: el nuevo registro para la relación «ticket» viola la restricción check «ticket_chk_reglas_web»
+
+-- VENTA WEB MIXTA (Inválido)
+INSERT INTO Ticket (FechaPago, HoraPago, TipoVenta, IdSucursal, IdCliente, EsTicketConsulta, EsTicketMedicamento)
+VALUES (CURRENT_DATE, CURRENT_TIME, 'Web', 1, 1, TRUE, TRUE);
+
+-- Resultado esperado: PostgreSQL también abortará la operación indicando que se violó la restricción «ticket_chk_reglas_web».
+
+-- VENTA PRESENCIAL MIXTA (Válido)
+INSERT INTO Ticket (FechaPago, HoraPago, TipoVenta, IdSucursal, IdCliente, EsTicketConsulta, EsTicketMedicamento)
+VALUES (CURRENT_DATE, CURRENT_TIME, 'Presencial', 1, 1, TRUE, TRUE);
+
+-- Resultado esperado: La consulta se ejecuta perfectamente.
+
+
+--====================================================================================================
+--                     Pruebas para el primer disparador del proyecto final
+--====================================================================================================
+
+-- Vender el medicamento controlado (ID 2) en el ticket presencial (Folio 1)
+INSERT INTO TenerMedComercial (FolioTicket, IdMedicamento, CantidadComprada, PrecioUnitario)
+VALUES (1, 2, 1, 250.00);
+
+-- RESULTADO ESPERADO: Éxito. (Query returned successfully).
+
+-- Vender el medicamento de venta libre (ID 1) en el ticket web (Folio 2)
+INSERT INTO TenerMedComercial (FolioTicket, IdMedicamento, CantidadComprada, PrecioUnitario)
+VALUES (2, 1, 3, 50.00);
+
+-- RESULTADO ESPERADO: Éxito. (Query returned successfully).
+
+-- Intentar vender el medicamento controlado (ID 2) en el ticket web (Folio 2)
+INSERT INTO TenerMedComercial (FolioTicket, IdMedicamento, CantidadComprada, PrecioUnitario)
+VALUES (2, 2, 1, 250.00);
+
+-- RESULTADO ESPERADO: PostgreSQL detiene la transacción inmediatamente y lanza el error:
+-- ERROR: Venta web rechazada: El medicamento ID 2 no es de venta libre (Tipo: Psicotrópico).
+
+--====================================================================================================
+--                     Pruebas para el primer procedimiento del proyecto final
+--====================================================================================================
+
+-- ======== Paso 1: Preparar las ventas del día (Datos de Prueba) ========
+
+-- 1. Creamos 3 ticket para el día de hoy
+INSERT INTO Ticket (FechaPago, HoraPago, TipoVenta, IdSucursal, IdCliente, EsTicketConsulta, EsTicketMedicamento)
+VALUES (CURRENT_DATE, CURRENT_TIME, 'Presencial', 1, 1, FALSE, TRUE);
+
+INSERT INTO Ticket(FechaPago,HoraPago,TipoVenta,IdSucursal,IdCliente,EsTicketConsulta,EsTicketMedicamento) 
+VALUES (CURRENT_DATE, CURRENT_TIME,'Web',1,1,FALSE,TRUE);
+
+INSERT INTO Ticket(FechaPago,HoraPago,TipoVenta,IdSucursal,IdCliente,EsTicketConsulta,EsTicketMedicamento) 
+VALUES (CURRENT_DATE, CURRENT_TIME,'Presencial',1,1,FALSE,TRUE);
+
+-- ==== Medicamentos Comerciales para el primer ticket====
+-- Agregamos el primer producto (Ej. 2 cajas de $50.00 = $100.00)
+INSERT INTO TenerMedComercial (FolioTicket, IdMedicamento, CantidadComprada, PrecioUnitario)
+VALUES (1, 1, 2, 50.00);
+
+-- Agregamos un segundo producto (Ej. 1 caja de $150.00 = $150.00)
+INSERT INTO TenerMedComercial (FolioTicket, IdMedicamento, CantidadComprada, PrecioUnitario)
+VALUES (1, 2, 1, 150.00);
+
+-- ==== Medicamentos Preparados para el segundo ticket====
+-- Agregamos el primer producto (Ej. 2 cajas de $25.00 = $50.00)
+INSERT INTO TenerMedPreparado (FolioTicket, IdMedicamento, CantidadComprada, PrecioUnitario)
+VALUES (2, 1, 2, 25.00);
+
+-- Agregamos un segundo producto (Ej. 1 caja de $50.00 = $50.00)
+INSERT INTO TenerMedPreparado (FolioTicket, IdMedicamento, CantidadComprada, PrecioUnitario)
+VALUES (2, 2, 1, 50.00);
+
+-- ==== Sólo Consulta para el tercer ticket====
+INSERT INTO CobrarConsulta(Fecha,Hora,Diagnostico,Precio,IdCliente,RFCMedico,RFCEnfermero,FolioTicket) 
+VALUES ('2024-01-18','2:00','Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.',100,3,'MBMO380825M20',NULL,3);
+
+-- SUMA ESPERADA PARA EL CORTE DE CAJA: $450.00 
+
+-- ======== Paso 2: Ejecutar el Procedimiento Almacenado ========
+-- Llamar al procedimiento de Corte de Caja para la fecha actual
+CALL sp_corte_caja_diario(CURRENT_DATE);
+
+
+--====================================================================================================
+--                     Pruebas para el segundo procedimiento del proyecto final
+--====================================================================================================
+CALL sp_reporte_stock_critico(10);
+
+
+--====================================================================================================
+--                     Pruebas para la primera función del proyecto final
+--====================================================================================================
+SELECT * FROM fn_calcular_cuenta_ticket(1); -- Sustituir el 1 por el folio de prueba
